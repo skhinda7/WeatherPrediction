be61e0a718c722151cb872c5a00c4ff73ad261a6
Simar Khinda <skhinda7@gmail.com>
2023-03-22 23:57:02 -0500

    Testing Server

---
+++ WeatherPredictionServer/src/FileRead.java
@@ -0,0 +1,11 @@
+public class FileRead {
+    public double[] earlyDataMin() {
+        double[] earlyDataMin = { 39, 49, 55, 54, 64, 48, 52, 53, 48, 52 };
+        return earlyDataMin;
+    }
+
+    public double[] earlyDataMax() {
+        double[] earlyDataMax = { 77, 77, 77, 78, 78, 78, 79, 79, 79, 80, 80, 80, 80 };
+        return earlyDataMax;
+    }
+}
---
+++ WeatherPredictionServer/src/GenericPredictor.java
@@ -0,0 +1,9 @@
+public class GenericPredictor {
+    protected double[] inputData;
+    protected double[] predictedData;
+
+    public void predict(double[] earlyData) {
+        double[] test = { 1.2, 3.4 };
+    }
+
+}
