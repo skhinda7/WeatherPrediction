fe0ff9f642186211c599b226073d7704a594b2e3
Simar Khinda <skhinda7@gmail.com>
2023-03-23 00:00:51 -0500

    Deleted Server file

--- WeatherPredictionServer/src/GenericPredictor.java
+++
@@ -1,9 +0,0 @@
-public class GenericPredictor {
-    protected double[] inputData;
-    protected double[] predictedData;
-
-    public void predict(double[] earlyData) {
-        double[] test = { 1.2, 3.4 };
-    }
-
-}
