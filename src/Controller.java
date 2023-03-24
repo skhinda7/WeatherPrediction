dd1891e61a71c3e898bc9c45d24b9001d4835f5d
Simardeep Khinda <73970774+skhinda7@users.noreply.github.com>
2023-03-24 01:39:42 -0400

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,44 @@
+//*********************************************************
+// Class: Controller 
+// Author: Simardeep Khinda
+// Created: 03/21/2023
+// Modified: 
+//
+// Purpose: Controller class calls other classes and methods to predict the weather data.
+//
+// Attributes: 
+//			-newMaxData: double[]
+//	        -newMinData: double[]
+// 
+// Methods: +main(String[] args): void
+//          -readTemp(): void
+//          -readData(): void
+//          -writeToFile(): void
+//          -minMaxAverage(double[], double[]): void
+//
+//*********************************************************
+
 public class Controller {
+    private double[] newMaxData;
+    private double[] newMinData;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    private void readTemp() {
+
+    }
+
+    private void readData() {
+
+    }
+
+    private void writeToFile() {
+
+    }
+
+    private void minMaxAverage(double[] LRData, double[] DMData) {
+
     }
 }
