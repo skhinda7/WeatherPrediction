efed3591cdfaa435cedeae4cb62c5bbd08458bd0
Simardeep Khinda <73970774+skhinda7@users.noreply.github.com>
2023-03-24 01:39:07 -0400

    Update and rename FileRead.java to FileManager.java

---
+++ src/FileManager.java
@@ -0,0 +1,36 @@
+//*********************************************************
+// Class: FileManager
+// Author: Simardeep Khinda
+// Created: 03/21/2023
+// Modified: 
+//
+// Purpose: FileManager is responsible for reading and writing to and from a file.
+//
+// Attributes: 
+//			-fileName: String
+//          -fileReadData: double[]
+// 
+// Methods: -readTemp(boolean dataIsMin): double[]
+//          -readData(): double[]
+//          -writeData(): void
+//
+//*********************************************************
+
+public class FileManager {
+    private String fileName;
+    private double[] fileReadData;
+
+    private double[] readTemp(boolean dataIsMin) {
+
+        return fileReadData;
+    }
+
+    private double[] readData() {
+
+        return fileReadData;
+    }
+
+    private void writeData(double[] data) {
+
+    }
+}
